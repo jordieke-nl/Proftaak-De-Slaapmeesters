54f22625bd8d196fefd9df1826fac6b0a35b8031
Unknown <63389@rocteraa-student.nl>
2018-05-15 12:22:06 +0200

    Favicon gemaakt

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link rel="icon" href="img/logo.png" type="img/logo.jpg"/>
     <meta charset="utf-8">
-    <title></title>
+    <title>De Slaapmeesters</title>
   </head>
   <body>
 
