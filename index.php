b292673868d6643a416d8188c50d86ef1f201924
Unknown <63389@rocteraa-student.nl>
2018-05-17 09:56:51 +0200

    Lay-out

--- index.php
+++ index.php
@@ -1,11 +1,132 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+
     <link rel="icon" href="img/logo.png" type="img/logo.jpg"/>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/css/mdb.min.css" rel="stylesheet">
     <title>De Slaapmeesters</title>
   </head>
   <body>
+    <div class="container">
+  <div class="jumbotron">
+   <img class="img-responsive logo" width="700" height="350" src="img/deslaapmeesters.png">
+    <p>Ervaring van toen, kennis van nu en een scherpe blik op de toekomst.</p>
+  </div>
+  <p>This is some text.</p>
+  <p>This is another text.</p>
+</div>
+<!--Footer-->
+      <footer class="page-footer font-medium stylish-color-dark pt-4 mt-4">
 
+          <!--Footer Links-->
+          <div class="container text-center text-md-left">
+
+              <!-- Footer links -->
+              <div class="row text-center text-md-left mt-3 pb-3">
+
+                  <!--First column-->
+                  <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                      <h4 class="text-uppercase mb-4 font-weight-bold">De Slaapmeesters</h4>
+                      <h5 >Ervaring van toen, kennis van nu en een scherpe blik op de toekomst.</h5>
+                        <h5>Wij staan voor:
+                          <li>Maatwerk</li>
+                          <li>Vakmanschap</li>
+                          <li>Korte levertijden</li>
+                        </h5>
+                  </div>
+                  <!--/.First column-->
+
+                  <hr class="w-100 clearfix d-md-none">
+
+                  <!--Second column-->
+
+                  <!--/.Second column-->
+
+                  <hr class="w-100 clearfix d-md-none">
+
+                  <!--Third column-->
+                  <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                      <h4 class="text-uppercase mb-4 font-weight-bold">Menu</h4>
+                      <h5><a href="#!">Home</a></p>
+                      <p><a href="#!">Over ons</a></p>
+                      <p><a href="#!">Producten</a></p>
+                      <p><a href="#!">Contact</a></p>
+                  </div>
+                  <!--/.Third column-->
+
+                  <hr class="w-100 clearfix d-md-none">
+
+                  <!--Fourth column-->
+                  <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                      <h4 class="text-uppercase mb-4 font-weight-bold">....</h4>
+                      <h5>....</p>
+                      <p>....</p>
+                      <p>....</p>
+                      <p>....</p>
+                  </div>
+                  <!--/.Fourth column-->
+
+                  <hr class="w-100 clearfix d-md-none">
+
+                  <!--Fourth column-->
+                  <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                      <h4 class="text-uppercase mb-4 font-weight-bold">Contact</h4>
+                      <h5><i class="fa fa-home mr-3"></i> Holtmeulen 3a, 5841 BX Oploo</p>
+                      <p><i class="fa fa-envelope mr-3"></i> info@deslaapmeesters.nl</p>
+                      <p><i class="fa fa-phone mr-3"></i> 0485-209003</p>
+                      <p><i class="fa fa-print mr-3"></i> www.deslaapmeesters.nl<p>
+                  </div>
+                  <!--/.Fourth column-->
+
+              </div>
+              <!-- Footer links -->
+
+              <hr>
+
+              <div class="row py-3 d-flex align-items-center">
+
+                  <!--Grid column-->
+                  <div class="col-md-8 col-lg-8">
+
+                      <!--Copyright-->
+                      <h5 class="text-center text-md-left grey-text">© 2018 Copyright:</p>
+                      <!--/.Copyright-->
+
+                  </div>
+                  <!--Grid column-->
+
+                  <!--Grid column-->
+                  <div class="col-md-3 col-lg-3 ml-lg-3">
+
+                      <!--Social buttons-->
+                      <div class="text-center text-md-right">
+                          <ul class="social list-unstyled list-inline">
+                            <a href="#" class="mr-3 fa fa-facebook"></a>
+                            <a href="#" class="mr-3 fa fa-instagram"></a>
+                            <a href="#" class="mr-3 fa fa-pinterest"></a>
+                          </ul>
+                      </div>
+                      <!--/.Social buttons-->
+
+                  </div>
+                  <!--Grid column-->
+
+              </div>
+
+          </div>
+
+      </footer>
+      <!--/.Footer-->
   </body>
 </html>
